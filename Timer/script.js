7892651ea5fe26c57ccd1509e40fbaa37b12d1e9
An Nguyen <an.nguyen@journeyh.io>
2021-11-11 14:52:40 +0700

    [Timer] [Feat] Add remain list & check correct of answer
    Add a list of remaining questions so do not choose the solved question
    
    Using debounce for checking the correctness of answer right after user
    finish typing

--- Timer/script.js
+++ Timer/script.js
@@ -4,37 +4,37 @@
       question: `A brother and a sister were born in summer and in winter.
       The sister was not born in winter.
       Who was born in summer?`,
-      answer: "The sister",
+      answer: "the sister",
       timeForAnswer: 20, //s
     },
     {
       question: `Olive's mom has five daughters:
       Bella, Annie, Martha, Kate...
       What is the name of the fifth daughter?`,
-      answer: "Olive",
+      answer: "olive",
       timeForAnswer: 30, //s
     },
     {
       question: `There are two ducks in front of a duck, two ducks behind a duck and a duck in the middle. How many ducks are there?`,
-      answer: "Three",
+      answer: "three",
       timeForAnswer: 20, //s
     },
     {
       question:
         "Five people were eating apples, A finished before B, but behind C. D finished before E, but behind B. What was the finishing order?",
-      answer: "CABDE",
+      answer: "cabde",
       timeForAnswer: 30, //s
     },
     {
       question:
         "Which country is the second-highest mountain on Earth belong to?",
-      answer: "Pakistan/China",
+      answer: "pakistan",
       timeForAnswer: 20, //s
     },
     {
       question:
         "The day before two days after the day before tomorrow is Saturday. What day is it today?",
-      answer: "Friday",
+      answer: "friday",
       timeForAnswer: 40, //s
     },
     {
@@ -44,33 +44,70 @@
     },
   ];
 
-  const timer = document.getElementById("timer-content");
+  const timerContent = document.getElementById("timer-content");
   // const timerClock = document.querySelector(".timer__clock-container");
   const timerStartBtn = document.querySelector(".timer__start-btn");
+  const answerInput = document.getElementById("answer");
   let selectedQuestion = {};
   //let countdownTime = 0;
   let countdownClock;
+  //array to hold index of remain question
+  let indexOfRemainQuestions = Array.from(
+    Array(questionAndAnswerPairs.length).keys()
+  );
 
   addWebsiteEventHandlers();
 
   function addWebsiteEventHandlers() {
     timerStartBtn.addEventListener("click", startCountdownClock);
+    answerInput.addEventListener(
+      "keyup",
+      debounceAnswerChange(function calback(e) {
+        checkCorrectnessOfAnswer(e);
+      })
+    );
   }
 
   function startCountdownClock() {
     const questionContainer = document.querySelector(".queston-container");
-    const selectedQuestion = randomSelectQuestion();
+    selectedQuestion = randomSelectQuestion();
     const countdownTime = selectedQuestion.timeForAnswer;
     questionContainer.innerHTML = selectedQuestion.question;
-    timer.style.display = "flex";
+    timerContent.style.display = "flex";
     timerStartBtn.style.display = "none";
     countdownClock = countdownInterval(countdownTime);
   }
 
+  function debounceAnswerChange(callback, timeout = 500) {
+    let timer;
+    return function holdTimer(...args) {
+      //args take argument pass in when event handle active, in this case keyboard event
+      //clearTimeOut to start new process
+      clearTimeout(timer);
+      //set time debounce invoked callback func when nothing change
+      timer = setTimeout(() => {
+        callback.apply(null, args);
+      }, timeout);
+    };
+  }
+
+  function checkCorrectnessOfAnswer(e) {
+    const answer = e.target.value.trim().toLowerCase();
+    const correctAnswer = selectedQuestion?.answer;
+    if (correctAnswer === answer) {
+      alert(true);
+    } else {
+      alert(false);
+    }
+  }
+
   function randomSelectQuestion() {
-    const randomQuestionIndex = Math.floor(
+    const randomIndex = Math.floor(
       Math.random() * questionAndAnswerPairs.length
     );
+    const randomQuestionIndex = indexOfRemainQuestions[randomIndex];
+    //remove selected quuestion out of remain question list
+    indexOfRemainQuestions.splice(randomIndex, 1);
     return questionAndAnswerPairs[randomQuestionIndex];
   }
 
@@ -98,7 +135,6 @@
 
   function convertTimeToMinuteAndSecond(time) {
     let minute = Math.floor(time / 60);
-    console.log(typeof time, minute * 60);
     let second = time - minute * 60;
     return [minute, second];
   }
