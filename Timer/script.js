76efd6e6ad0c57915ee54b082c8d12efea770d9c
An Nguyen <an.nguyen@journeyh.io>
2021-11-11 10:27:31 +0700

    [Timer] [Feat] Add question list and display question
    Add question and answer pairs and random select question funciton
    
    Add event handler to display question in UI

---
+++ Timer/script.js
@@ -0,0 +1,69 @@
+(function operation() {
+  const questionAndAnswerPairs = [
+    {
+      question: `A brother and a sister were born in summer and in winter.
+      The sister was not born in winter.
+      Who was born in summer?`,
+      answer: "The sister",
+      timeForAnswer: "10000", //ms
+    },
+    {
+      question: `Olive's mom has five daughters:
+      Bella, Annie, Martha, Kate...
+      What is the name of the fifth daughter?`,
+      answer: "Olive",
+      timeForAnswer: "15000", //ms
+    },
+    {
+      question: `Input the missing number
+      2,32,332,...,33332`,
+      answer: "3332",
+      timeForAnswer: "10000", //ms
+    },
+    {
+      question: "What is your main focus today?",
+      answer: "complete timer website",
+      timeForAnswer: "120000", //ms
+    },
+    {
+      question:
+        "Which country is the second-highest mountain on Earth belong to?",
+      answer: "Pakistan/China",
+      timeForAnswer: "20000", //ms
+    },
+    {
+      question: "How long will it take to sell 1 billion packets of sesame?",
+      answer: "guess what?",
+      timeForAnswer: "10000", //ms
+    },
+  ];
+  const minuteInTimer = document.getElementById("minute-timer");
+  const secondInTimer = document.getElementById("second-timer");
+  const timer = document.querySelector(".timer");
+  let selectedQuestion = {};
+  let countdownTime = 0;
+  let countdownClockDisplay;
+
+  addWebsiteEventHandlers();
+
+  function addWebsiteEventHandlers() {
+    const startBtn = document.getElementById("start-btn");
+    startBtn.addEventListener("click", startCountdownClock);
+  }
+
+  function randomSelectQuestion() {
+    const randomQuestionIndex = Math.floor(
+      Math.random() * questionAndAnswerPairs.length
+    );
+    return questionAndAnswerPairs[randomQuestionIndex];
+  }
+
+  function startCountdownClock() {
+    const questionContainer = document.querySelector(".queston-container");
+    const selectedQuestion = randomSelectQuestion();
+    countdownTime = selectedQuestion.timeForAnswer;
+    questionContainer.innerHTML = selectedQuestion.question;
+    timer.style.display = "flex";
+    console.log(countdownTime);
+  }
+})();
